7395c01e966580320c09f81b11d0026ebd16a431
Vladimir Khuraskin <vladimirkhuraskin@appbooster.ru>
2019-02-05 16:16:39 +0400

    Initial Commit

---
+++ Restaurant/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Restaurant
+//
+//  Created by Vladimir Khuraskin on 05/02/2019.
+//  Copyright © 2019 Vladimir Khuraskin. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+  override func viewDidLoad() {
+    super.viewDidLoad()
+    // Do any additional setup after loading the view, typically from a nib.
+  }
+
+
+}
+
